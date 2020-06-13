1294ddd8b0a7e5b12605a4f82623035de656ffd6
LeiYangGH <675686066@qq.com>
2020-06-13 20:31:15 +0800

    tested all cols

--- sheet_split.py
+++ sheet_split.py
@@ -1,21 +1,35 @@
 import pandas as pd
 import os
 
-base_path = r'C:\Users\LeiYang\Downloads'
-excel_file = os.path.join(base_path, '111.xlsx')
-all_df = pd.read_excel(excel_file, header=[0])
-# all_df.to_excel(os.path.join(base_path, '222.xlsx'))
-countries = set(all_df['Winner']).union(set(all_df['Loser']))
-# print(all_df)
-# print(all_df.columns.tolist())
-print(countries)
-countries_folder = os.path.join(base_path, f'countries')
-if not os.path.isdir(countries_folder):
-    os.mkdir(countries_folder)
-for country in countries:
-    # print(country)
-    country_df = all_df[(all_df['Winner'] == country) | (all_df['Loser'] == country)]  #
-    print(len(country_df))
-    country_file = os.path.join(countries_folder, f'{country}.xlsx')
-    print(country_file)
-    country_df.to_excel(country_file)
+
+def split_excel_by_column_values(base_path=os.getcwd()
+                                 , input_excel=None,
+                                 columns=['Winner', 'Loser']):
+    '''base_path:工作目录，如果不设定则假定为当前目录
+    input_excel: excel文件名（不包含文件夹），如果不设定则扫描base_path里的第一个excel文件
+    columns: 以哪些列的值为拆分的文件名
+    '''
+
+    if not input_excel:
+        input_excel = next(f for f in os.listdir(base_path) if f.endswith('.xlsx'))
+    excel_file = os.path.join(base_path, input_excel)
+    all_df = pd.read_excel(excel_file, header=[0])
+    # all_df.to_excel(os.path.join(base_path, '222.xlsx'))
+    groupkeys = set()
+    for col in columns:
+        groupkeys = groupkeys.union(set(all_df[col]))
+    # print(all_df)
+    print(all_df.columns.tolist())
+    print(groupkeys)
+    countries_folder = os.path.join(base_path, 'countries')
+    if not os.path.isdir(countries_folder):
+        os.mkdir(countries_folder)
+    for key in groupkeys:
+        key_df = all_df[(all_df['Winner'] == key) | (all_df['Loser'] == key)]  #
+        key_file = os.path.join(countries_folder, f'{key}.xlsx')
+        key_df.to_excel(key_file)
+        print(f'{key}共{len(key_df)}条记录已保存到{key_file}')
+
+
+if __name__ == '__main__':
+    split_excel_by_column_values(base_path=r'C:\Users\LeiYang\Downloads')
