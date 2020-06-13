7d89e07b0dd64a18dd52f50e6835ce35b9cf5888
LeiYangGH <675686066@qq.com>
2020-06-13 16:16:26 +0800

    can filter

--- sheet_split.py
+++ sheet_split.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import os
 
-excel_file = r'C:\Users\LeiYang\Downloads\matches.xlsx'
-all_df = pd.read_excel(excel_file)
-countries = set(all_df['colb'])
+base_path = r'C:\Users\LeiYang\Downloads'
+excel_file = os.path.join(base_path, '111.xlsx')
+all_df = pd.read_excel(excel_file, header=[0])
+countries = set((all_df['Winner'] + all_df['Loser']))
 print(all_df)
 print(countries)
+countries_folder = os.path.join(base_path, f'countries')
+if not os.path.isdir(countries_folder):
+    os.mkdir(countries_folder)
 for country in countries:
-    print(country)
-    country_df = all_df
+    # print(country)
+    country_df = all_df[(all_df['Winner'] == country) | (all_df['Loser'] == country)]
+    country_file = os.path.join(countries_folder, f'{country}.xlsx')
+    print(country_file)
+    country_df.to_excel(country_file)
