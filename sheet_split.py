272a394f50d2ec070c3168bafd6d607d7ac8a614
LeiYangGH <675686066@qq.com>
2020-06-13 20:37:45 +0800

    interaction comments

--- sheet_split.py
+++ sheet_split.py
@@ -11,20 +11,24 @@ def split_excel_by_column_values(base_path=os.getcwd()
     '''
 
     if not input_excel:
-        input_excel = next(f for f in os.listdir(base_path) if f.endswith('.xlsx'))
+        input_excel = next((f for f in os.listdir(base_path) if f.endswith('.xlsx')), None)
+    if not input_excel:
+        print(r'在路径{base_path}下未找到任何.xlsx文件，请检查!')
+        return
     excel_file = os.path.join(base_path, input_excel)
     all_df = pd.read_excel(excel_file, header=[0])
+    print(f'成功读取输入文件{excel_file}')
     # all_df.to_excel(os.path.join(base_path, '222.xlsx'))
-    groupkeys = set()
+    group_keys = set()
     for col in columns:
-        groupkeys = groupkeys.union(set(all_df[col]))
+        group_keys = group_keys.union(set(all_df[col]))
     # print(all_df)
-    print(all_df.columns.tolist())
-    print(groupkeys)
+    # print(all_df.columns.tolist())
+    # print(group_keys)
     countries_folder = os.path.join(base_path, 'countries')
     if not os.path.isdir(countries_folder):
         os.mkdir(countries_folder)
-    for key in groupkeys:
+    for key in group_keys:
         key_df = all_df[(all_df['Winner'] == key) | (all_df['Loser'] == key)]  #
         key_file = os.path.join(countries_folder, f'{key}.xlsx')
         key_df.to_excel(key_file)
